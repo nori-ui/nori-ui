294179a9827d772a7fca120d0e08b3a981f4603e
Manuel Bieh <git@manuelbieh.de>
2026-05-05 03:22:14 +0200

    feat(core): export Calendar from package root with compound slots

---
+++ packages/core/src/components/Calendar/index.ts
@@ -0,0 +1,11 @@
+export { Calendar } from './Calendar';
+export type {
+    CalendarBehavior,
+    CalendarMode,
+    CalendarProps,
+    CalendarValue,
+    CalendarView,
+    ChangeMeta as CalendarChangeMeta,
+    DateRange,
+    DayContext,
+} from './Calendar.types';
