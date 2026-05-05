a817455c4c5043e9c43df8a01cfe1754d02f8894
Manuel Bieh <git@manuelbieh.de>
2026-05-05 12:09:50 +0200

    fix(core): re-export SliderGestureProvider and useSliderInteractionActive

--- packages/core/src/components/Slider/index.ts
+++ packages/core/src/components/Slider/index.ts
@@ -1,1 +1,2 @@
 export { Slider, type SliderDirection, type SliderOrientation, type SliderProps } from './Slider';
+export { SliderGestureProvider, useSliderInteractionActive } from './slider-gesture-context';
